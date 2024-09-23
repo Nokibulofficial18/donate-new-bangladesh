29b67921ca1a0f2d062891f9068fed946071ed08
Nokibulofficial18 <nokibuldragon@gmail.com>
2024-09-23 20:56:47 +0600

    done functions and donation tasks

---
+++ javascript/app.js
@@ -0,0 +1,28 @@
+// To maintain the balance of the donation and the total balance of the user
+function addDonation(input_field_id, donation_id, donation_type) {
+    let have_balance = getTextFieldValueByID('total-balance');
+    let donation_balance = getTextFieldValueByID(donation_id);
+
+    const inputAmount = getInputFieldValueByID(input_field_id);
+
+    if (inputAmount === 0) {
+        alert('Please enter an amount!');
+        return;
+    }
+
+    if (isNaN(inputAmount) || inputAmount < 0) {
+        alert('Please enter a valid amount!')
+        return;
+    }
+
+
+    if (inputAmount > have_balance) {
+        alert(`You don't have sufficient balance!`)
+        return;
+    }
+
+    setTextFieldValueByID('total-balance', have_balance - inputAmount);
+    setTextFieldValueByID(donation_id, donation_balance + inputAmount);
+    successfulDonation();
+    makeHistory(donation_type, inputAmount);
+}
---
+++ javascript/functions.js
@@ -0,0 +1,43 @@
+// to get input field value in number format
+function getInputFieldValueByID(id) {
+    const element = document.getElementById(id);
+    const balance = Number(element.value);
+    element.value = '';
+    return balance;
+}
+
+// to get text field value in number format
+function getTextFieldValueByID(id) {
+    const element = document.getElementById(id).innerText;
+    const balance = Number(element);
+    console.log(balance);
+    return balance;
+}
+
+// to set text field value
+function setTextFieldValueByID(id, amount) {
+    const element = document.getElementById(id);
+    element.innerText = amount;
+}
+
+// to show to successful donation modal
+function successfulDonation() {
+    document.getElementById('successful-donation').showModal();
+}
+
+// to toggle a element visibility
+function hideShow(id) {
+    const element = document.getElementById(id);
+    if (element.classList.contains('hidden'))
+        element.classList.remove('hidden');
+    else
+        element.classList.add('hidden');
+}
+
+// Function to set attributes and inner content of newly donation records
+function historyMakeHelper(newHistory, idName, serial, content) {
+    const element = newHistory.querySelector(`#${idName}`);
+    element.setAttribute('id', `${idName}-${serial}`);
+    if(content !== '')
+        newHistory.querySelector(`#${idName}-${serial}`).innerText = content;
+}
